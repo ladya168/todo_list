860a7edaf57a23f7abc07948b3992dcb1e137d6f
ladya168 <abemenegr22815@gmail.com>
2026-06-07 16:54:01 +1000

    add CORS

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 from fastapi import FastAPI
 from routers import auth, notes
 from service import lifespan
+from fastapi.middleware import CORSMiddleware
 
 
 
 
 
 
-# config = AuthXConfig()
 app = FastAPI(lifespan=lifespan)
-
-# config.JWT_SECRET_KEY = settings.secret_key
-# config.JWT_ALGORITHM = settings.algorithm
-# config.JWT_ACCESS_COOKIE_NAME = "access_token"
-# config.JWT_TOKEN_LOCATION = ["cookies"]
 
 
 app.include_router(auth.router)
 app.include_router(notes.router)
 
-
-
-# sequrity = AuthX(config = config)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
